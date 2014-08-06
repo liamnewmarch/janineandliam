0cd9e82576e4d0da75f687cd2261e4f92a55aa7c
Liam Newmarch <liamnewmarch@github.users.noreply.com>
2014-08-06 13:22:11 +0100

    Convert to Jekyll build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,46 +3,28 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		clean: {
-			main: [ 'dist/' ]
-		},
-
-		copy: {
-			main: {
-				files: [{
-					expand: true,
-					flatten: true,
-					src: [ 'src/*.html' ],
-					dest: 'dist/',
-					filter: 'isFile'
-				}]
-			}
-		},
-
 		sass: {
 			main: {
 				options: {
 					style: 'compressed'
 				},
 				files: {
-					'dist/assets/css/main.css': 'src/assets/css/main.scss'
+					'assets/css/main.css': 'assets/css/main.scss'
 				}
 			}
 		},
 
 		autoprefixer: {
 			main: {
-				 src: 'dist/assets/css/main.css'
+				 src: 'assets/css/main.css'
 			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 
 	grunt.registerTask('default', [
-		'clean', 'copy', 'sass', 'autoprefixer'
+		'sass', 'autoprefixer'
 	]);
 };
