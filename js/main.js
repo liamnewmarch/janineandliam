7b7d636f60099df059efbb48e26de362cbebe7de
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-07-07 18:24:06 +0100

    I forgot this existed! Clean up, remove CNAME

--- js/main.js
+++ js/main.js
@@ -1,38 +1,34 @@
-class DaysAgo extends HTMLElement {
-  static get is() {
-    return 'days-ago';
-  }
+customElements.define('days-ago', class extends HTMLElement {
+  static tagName = 'days-ago'
 
-  static get observedAttributes() {
-    return ['since'];
-  }
+  static observedAttributes = ['since']
 
-  get days() {
-    const today = new Date();
+  #calculateDays() {
+    const today = new Date()
     const since = new Date(this.since)
-    return Math.floor((today - since) / 864e5);
+    return Math.floor((today - since) / 864e5)
+  }
+
+  #format() {
+    const days = this.#calculateDays()
+    const years = days / 365.25
+    return `${years.toFixed(1)} years or ${days.toLocaleString()} days`
   }
 
   get since() {
-    return this.getAttribute('since');
+    return this.getAttribute('since')
   }
 
   set since(value) {
-    this.setAttribute('since', value);
+    this.setAttribute('since', value)
   }
 
-  constructor(...args) {
-    super(...args);
-    this.attachShadow({ mode: 'open' });
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
   }
 
-  attributeChangedCallback(key, _, value) {
-    if (key === 'since') {
-      this.shadowRoot.textContent = this.days;
-    }
+  attributeChangedCallback(key) {
+    this.shadowRoot.textContent = this.#format()
   }
-}
-
-if ('customElements' in window) {
-  customElements.define(DaysAgo.is, DaysAgo);
-}
+})
